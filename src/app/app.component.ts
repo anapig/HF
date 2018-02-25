c272c77d74e9599ff5d1b43447984cb51e781846
anapig <anass.Dennaye@gmail.com>
2018-02-25 12:35:27 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello anass';
-
-  personne = {nom:"toto" , email:"toto@toto.to"}
+  
 }
